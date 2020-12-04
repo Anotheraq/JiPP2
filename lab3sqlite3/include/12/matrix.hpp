996f1229ea39caca9ba764e5a1621025c96d5fa7
Anotheraq <62223979+Anotheraq@users.noreply.github.com>
2020-12-04 19:08:33 +0100

    Added sqlite3 ver.

---
+++ lab3sqlite3/include/12/matrix.hpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include "sqlite3/sqlite3.h"
+
+
+class Matrix
+{
+    private:
+        double ** arr;
+        int n_class {}, m_class {};
+    public:
+        Matrix(int n, int m);
+        Matrix(int n);
+        Matrix(std::string filename, std::string path);
+        void set(int n, int m, double val);
+        double get(int x, int y);
+        Matrix add(Matrix m2);
+        Matrix subtract(Matrix m2);
+        Matrix multiply(Matrix m2);
+        int rows();
+        int cols();
+        void store(std::string filename,std::string path);
+        void print();
+};
---
+++ lab3sqlite3/src/matrix.cpp
@@ -0,0 +1,195 @@
+#include "matrix.hpp"
+
+Matrix::Matrix(int n, int m)
+{
+    n_class = n;
+    m_class = m;
+    arr = new double *[n_class];
+
+    for (int i = 0; i < n_class; i++)
+    {
+        arr[i] = new double[m_class];
+    }
+
+    for (int i = 0; i < n_class; i++)
+    {
+        for (int j = 0; j < m_class; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+}
+Matrix::Matrix(int n)
+{
+    m_class = n;
+    n_class = n;
+    
+    arr = new double *[n_class];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new double[n_class];
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+}
+Matrix *mdbcb;
+
+
+int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+    if(argv[2][0] == 'n')
+    {
+        mdbcb = new Matrix(atoi(argv[0]), atoi(argv[1]));
+    }else{
+        mdbcb -> set(atoi(argv[0]), atoi(argv[1]), atoi(argv[2]));
+    }
+    return 0;
+}
+
+Matrix::Matrix(std::string filename, std::string path)
+{
+    
+    std::string sql;
+    sqlite3 *dbs;
+    
+    std::string pn = path+filename + ".db";
+    sqlite3_open(pn.c_str(), &dbs);
+    
+    sql = "SELECT * FROM 'MATRIX'";
+    sqlite3_exec(dbs, sql.c_str(), callback, 0, 0);
+    
+    n_class = mdbcb->n_class;
+    m_class = mdbcb->m_class;
+    std::cout << n_class << m_class << std::endl;
+    arr = (double**)new double* [n_class];
+    for (int i = 0; i < n_class; i++)
+        arr[i] = (double*)new double[m_class];
+
+    for (int i = 0; i < n_class; i++)
+        for (int j = 0; j < m_class; j++)
+            arr[i][j] = mdbcb->get(i,j);
+}
+
+
+
+
+
+void Matrix::set(int n, int m, double val)
+{
+    arr[n][m] = val;
+}
+
+double Matrix::get(int n, int m)
+{
+    return arr[n][m];
+}
+
+int Matrix::rows()
+{
+    return n_class;
+}
+
+int Matrix::cols()
+{
+    return m_class;
+}
+
+Matrix Matrix::add(Matrix m2)
+{
+    Matrix madd(rows(),cols());
+    if (rows() == m2.rows() && cols() == m2.cols())
+    {
+        for (int i = 0; i < n_class; i++)
+        {
+            for (int j = 0; j < m_class; j++)
+            {
+                madd.arr[i][j] = arr[i][j] + m2.arr[i][j];
+            }
+        }
+        return madd;
+    }else{
+        std::cout << "Dodanie niemozliwe";
+        exit(1);
+    }
+}
+
+Matrix Matrix::subtract(Matrix m2)
+{
+    Matrix msub(rows(),cols());
+    if (rows() == m2.rows() && cols() == m2.cols())
+    {
+        for (int i = 0; i < n_class; i++)
+        {
+            for (int j = 0; j < m_class; j++)
+            {
+                msub.arr[i][j] = arr[i][j] - m2.arr[i][j];
+            }
+        }
+        return msub;
+    }else{
+        std::cout << "Odejmowanie niemozliwe";
+        exit(1);
+    }
+}
+
+Matrix Matrix::multiply(Matrix m2)
+{
+    Matrix mmul(rows(),cols());
+    if (cols() != m2.rows())
+    {
+        std::cout << "Mnozenie podanych macierzy nie jest mozliwe" << std::endl;
+        exit(1);
+    }
+    for (int i = 0; i < n_class; i++)
+    {
+        for (int j = 0; j < m_class; j++)
+        {
+            for (int k = 0; k < m_class; k++)
+                mmul.arr[i][j] += arr[i][k] * m2.arr[k][j];
+        }
+    }
+    return mmul;    
+}
+
+void Matrix::store(std::string filename,std::string path)
+{
+    std::string sql;
+    sqlite3 *dbs;
+    std::string pn = path+filename + ".db";
+
+    sqlite3_open(pn.c_str(), &dbs);
+        
+    sql = "CREATE TABLE MATRIX(n,m,v);";
+    sqlite3_exec(dbs, sql.c_str(), 0, 0, 0);
+    
+    sql = "INSERT INTO MATRIX ('n', 'm', 'v') VALUES ('" + std::to_string(rows()) + "','" + std::to_string(cols()) + "','n');";
+    sqlite3_exec(dbs, sql.c_str(), 0, 0, 0);
+    
+    for (int i = 0; i < rows(); i++)
+    {
+        for (int j = 0; j < cols(); j++)
+        {
+            sql = "INSERT INTO MATRIX ('n', 'm', 'v') VALUES ('" + std::to_string(i) + "','" + std::to_string(j) + "','" + std::to_string(arr[i][j]) + "');";
+            sqlite3_exec(dbs, sql.c_str(), 0, 0, 0);
+        }
+    }
+    sqlite3_close(dbs);
+}
+
+void Matrix::print()
+{
+    for (int i = 0; i < n_class; i++)
+    {
+        for (int j = 0; j < m_class; j++)
+        {
+            std::cout << std::setw(4) << get(i,j) << "  ";
+        }
+        std::cout << std::endl;
+    }
+}
