16fb8b7f3ac1c264c756241823f1294e5a4469a1
Anotheraq <62223979+Anotheraq@users.noreply.github.com>
2020-12-04 21:03:41 +0100

    Commit

--- lab3sqlite3/include/lab3sqlite3/matrix.hpp
+++ lab3sqlite3/include/lab3sqlite3/matrix.hpp
@@ -11,16 +11,21 @@ class Matrix
         double ** arr;
         int n_class {}, m_class {};
     public:
-        Matrix(int n, int m);
-        Matrix(int n);
-        Matrix(std::string filename, std::string path);
-        void set(int n, int m, double val);
-        double get(int x, int y);
-        Matrix add(Matrix m2);
-        Matrix subtract(Matrix m2);
-        Matrix multiply(Matrix m2);
-        int rows();
-        int cols();
-        void store(std::string filename,std::string path);
-        void print();
+        
+        Matrix(int n, int m);                           // konstruktor przyjmujący dwa argumenty typu int tworzący macierz o podanych wymiarach zainicjalizowaną zerami
+        Matrix(int n);                                  //konstruktor przyjmujący jeden argument typu int tworzący macierz kwadratową o podanym rozmiarze zainicjalizowaną zerami
+        Matrix(std::string filename, std::string path); //dodatkowy konstruktor jako argument przyjmujący ścieżkę do bazy dannych o podanym wcześniej formacie i na jego podstawie tworzący nową macierz na podstawie przekazanego pliku
+        
+        void set(int n, int m, double val); //metoda ustawiająca wartość elementu (n, m) na val
+        double get(int x, int y);           //metoda pobierająca element (n, m)
+       
+        Matrix add(Matrix m2);      //metoda przyjmująca jako argument inną macierz i zwracająca jako wynik nową macierz będącą sumą bieżącej macierzy oraz macierzy przekazanej jako argument
+        Matrix subtract(Matrix m2); //metoda przyjmująca jako argument inną macierz i zwracająca jako wynik nową macierz będącą różnicą bieżącej macierzy oraz macierzy przekazanej jako argument
+        Matrix multiply(Matrix m2); //metoda przyjmująca jako argument inną macierz i zwracająca jako wynik nową macierz będącą iloczynem bieżącej macierzy oraz macierzy przekazanej jako argumen
+       
+        int rows(); //metoda zwracająca liczbę wierszy macierzy
+        int cols(); //metoda zwracająca liczbę kolumn macierzy
+
+        void store(std::string filename,std::string path);//metoda zapisująca macierz w bazie dannych
+        void print();               //metoda wyświetlająca macierz na ekranie
 };
